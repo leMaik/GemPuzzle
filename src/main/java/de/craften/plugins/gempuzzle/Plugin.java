83f779a5b923213af35e7d89493433f7f19fcf4f
leMaik <an@maik.tk>
2016-03-10 20:19:51 +0100

    Add initial project files.

---
+++ src/main/java/de/craften/plugins/gempuzzle/Plugin.java
@@ -0,0 +1,9 @@
+package de.craften.plugins.gempuzzle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A plugin that adds gem puzzles (also known as 15 puzzles), using item frames.
+ */
+public class Plugin extends JavaPlugin {
+}
